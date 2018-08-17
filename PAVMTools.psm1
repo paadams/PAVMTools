3b0ee9dd7acd8ea13f06b3de471bfbe0cd2cb6cf
Peter Adams <paadams@gmail.com>
2018-08-17 11:19:19 -0500

    Version 1
    Version of my PAVMTools module

---
+++ PAVMTools.psm1
@@ -0,0 +1,49 @@
+$PAErrorLogPreference = 'C:\temp\pa-errors.log'
+function Get-PAVMwareInfo {
+
+<#
+.SYNOPSIS
+Gets VM hardware and other information from vCenter for a
+list of VMs. 
+.DESCRIPTION
+Queries hardware and other information from vCenter for VMs
+.PARAMETER ComputerName
+The name of the VM to query
+.EXAMPLE
+Get-VMinfo -ComputerName WHATEVER
+.EXAMPLE
+Get-Content c:\servers.txt | Get-VMinfo | Format-Table
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$True,
+              ValueFromPipeline=$True,
+              ValueFromPipelineByPropertyName=$True,
+              #ParamterSetName='computername',
+              HelpMessage="Enter a help message")]
+    [string[]]$ComputerName,
+
+    [Parameter()]
+    [string]$ErrorLogFilePath = $PAErrorLogPreference
+
+     )
+     Begin{}
+     PROCESS{
+            foreach($computer in $ComputerName){
+                $vm = Get-View -ViewType VirtualMachine -Filter @{"Name" = $computer}
+                $vmguest = Get-vm $computer 
+                $props = @{'ComputerName' = $vm.Name;
+                           'NumCPU' = $vm.config.hardware.NumCPU;
+                           'NumCores' = $vm.config.hardware.NumCoresPerSocket;
+                           'MemoryGB' = ($vm.config.hardware.MemoryMB / 1024);
+                           'OSVersion' = $vm.Guest.GuestFullName;
+                           'VMToolsVersion' = $vmguest.Guest.ToolsVersion;
+                           'ToolsStatus' = $vm.Guest.ToolsVersionStatus;
+                           'HardwareVersion' = $vm.Config.Version}                         
+                $obj = New-Object -TypeName psobject -Property $props
+                $obj.PSObject.TypeNames.Insert(0,'PA.VMwareInfo')
+                Write-Output $obj
+     }
+}
+     END{}
+}
